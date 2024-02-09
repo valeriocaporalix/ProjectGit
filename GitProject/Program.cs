722429627d09600cd1c5d89d051911f71950f44f
valeriocaporalix <cyndoriil@gmail.com>
2024-02-09 15:32:09 +0100

    program init

--- GitProject/Program.cs
+++ GitProject/Program.cs
@@ -1,3 +1,14 @@
 ﻿using GitProject.Models;
 
 var mario = new People() { Name = "Mario", LastName = "Rossi" };
+
+var input = "";
+
+while (input?.ToLower() != "esci")
+{
+    Console.WriteLine("Il programma è in esecuzione. Digita 'Esci' per uscire.");
+
+    input = Console.ReadLine();
+}
+
+Console.WriteLine("Il programma è stato terminato.");
